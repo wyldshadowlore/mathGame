a87ce1e25e5e91edc8f1d66e28e21756df77037a
WyldLore Collab <83235587+wyldshadowlore@users.noreply.github.com>
2021-04-28 19:10:27 -0500

    Update main.py
    Added menu and color.

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 """
 Main game file
 """
+#import essential files
+import os
+from rich import print
+
+
+def main():
+	os.system('cls')
+	menu()
+
+def menu():
+	print(f"""
+		[red]This game will help you with math.[/] 
+		
+		[green]Please choose:[/]
+		[blue](A)ddition
+		(S)ubtraction
+		(M)ultiplication
+		(D)ivion[/]
+		""")
+
+	mathChoice = input("Choose up to 4: ")
+	gamesChoice = input("How many problems would you like? (1 - 30)")
+
+
+if __name__ == "__main__":
+	main()
