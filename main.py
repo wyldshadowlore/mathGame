952efc8a2eb9d1540348d37834967ef2c7ff1504
WyldLore Collab <83235587+wyldshadowlore@users.noreply.github.com>
2021-04-28 21:22:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,47 @@ Main game file
 import os
 from rich import print
 
+validChoices = ['a', 's', 'd', 'm']
 
 def main():
-	os.system('cls')
 	menu()
 
 def menu():
-	print(f"""
-		[red]This game will help you with math.[/] 
-		
-		[green]Please choose:[/]
-		[blue](A)ddition
-		(S)ubtraction
-		(M)ultiplication
-		(D)ivion[/]
-		""")
+	menuLoop = True
+	while menuLoop:
+		os.system('cls')
+		print(f"""
+			[red]This game will help you with math.[/] 
+			
+			[green]Please choose:[/]
+			[blue](A)ddition
+			(S)ubtraction
+			(M)ultiplication
+			(D)ivion[/]
+			""")
 
-	mathChoice = input("Choose up to 4: ")
-	gamesChoice = input("How many problems would you like? (1 - 30)")
+
+		while True:
+			mathChoice = (input("Choose up to 4: (ASMD) "))
+			mathChoice = mathChoice.lower()
+			if any(elem not in validChoices for elem in list(mathChoice)):
+				print("Invalid choice.")
+			else:
+				break
+
+		while True:
+			try:
+				gamesChoice = int(input("How many problems would you like? (1 - 30)"))
+				break
+			except:
+				print("Invalid choice, please choose between 1 and 30.")
+		if gamesChoice >= 1 and gamesChoice <= 30:
+			print(gamesChoice, mathChoice)
+			print("Exiting Gracefully")
+			menuLoop = False
+		else:
+			pass
+
 
 
 if __name__ == "__main__":
