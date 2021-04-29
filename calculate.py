0ac8c59467d062926f168cfa74452e49a61800e4
WyldLore Collab <83235587+wyldshadowlore@users.noreply.github.com>
2021-04-29 07:03:18 -0500

    added SMD
    Added SMD,
    still need to get rid of rationals and integers.

--- calculate.py
+++ calculate.py
@@ -5,18 +5,45 @@ import random
 
 
 def nextProblem(mathChoice, validChoices):
-	getOperation = random.choices(list(mathChoice))  ## Need to change this back to string
-#	getOperation = ''.join(getOperation)
-	if getOperation == validChoices[0] :
-		augend = random.randrange(10)
-		addend = random.randrange(10)
-		print(augend, "+", addend, "=")
+	getOperation = random.choices(mathChoice)  ## grab a random choice
+	getOperation = ''.join(getOperation)  ## is there a cleaner way to get rand choice witout normalizing to str?
+	x0 = random.randrange(11)
+	x1 = random.randrange(11)
+
+	if getOperation == validChoices[0] : #addition -- seems legit
+		print(x0, "+", x1, "=")
 		getAnswer = int(input())
-		if (augend + addend) == getAnswer:
+		if (x0 + x1) == getAnswer:
 			pass
 		else:
 			pass
 
+	elif getOperation == validChoices[1] : #subtraction -- needs to be cleaned up to ensure no negative integers
+		print(x0, "-", x1, "=")
+		getAnswer = int(input())
+		if (x0 - x1) == getAnswer:
+			pass
+		else:
+			pass
+
+	elif getOperation == validChoices[2] : # division -- needs to be cleaned up to ensure no irrationals, etc.
+		print(x0, "/", x1, "=")
+		getAnswer = int(input())
+		if (x0 / x1) == getAnswer:
+			pass
+		else:
+			pass
+
+	elif getOperation == validChoices[3] : # multiplication -- seems legit
+		print(x0, "x", x1, "=")
+		getAnswer = int(input())
+		if (x0 * x1) == getAnswer:
+			pass
+		else:
+			pass
+
+	else:
+		pass
 
 def problemSet(mathChoice, gamesChoice, validChoices):
 	counter = 0
---
+++ t.py
@@ -0,0 +1,7 @@
+import random
+validChoices = ['a', 's', 'd', 'm']
+a = random.choices(validChoices)
+
+print(validChoices[0])
+print(validChoices[1])
+print(a)
